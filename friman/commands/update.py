8462a45dc4cf8ecf42755f76d93074eb83b48819
thelicato <thelicato@users.noreply.github.com>
2025-11-19 20:56:07 +0100

    feat: add 'update' command

---
+++ friman/commands/update.py
@@ -0,0 +1,27 @@
+import json
+from datetime import datetime
+import typer
+from friman.utils import helpers, definitions
+from friman.utils.logger import frimanlog
+from friman.utils.exceptions import RateLimitException
+
+app = typer.Typer()
+
+@app.command()
+def update():
+    """Update the local list of available Frida versions."""
+
+    frimanlog.info("Setting up the list of available frida versions...")
+    try:
+        current_config = helpers.get_current_config()
+        frida_tags = helpers.get_all_github_tags(definitions.FRIDA_REPO)
+        # Simulated update process
+        current_config["tags"] = frida_tags
+        current_config["updated_at"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        with open(definitions.FRIMAN_CONFIG_FILE, "w") as f:
+            json.dump(current_config, f, ensure_ascii=False, indent=2)
+        frimanlog.info(f"{definitions.FRIMAN_CONFIG_FILE} updated!")
+    except RateLimitException as ex:
+        frimanlog.error("A rate limit error was encountered while updating list of Frida tags.")
+    except Exception as ex:
+        frimanlog.error(f"An error occurred while updating the list of Frida tags: {ex}")
