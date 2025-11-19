711bff5c30ae43c4f8a5f1d71a5c931438c772ac
thelicato <thelicato@users.noreply.github.com>
2025-11-19 20:57:21 +0100

    feat: enhance helpers and defs

--- friman/utils/definitions.py
+++ friman/utils/definitions.py
@@ -15,5 +15,22 @@ FRIMAN_FOLDER = path.join(Path.home(), DEFAULT_BASE_PATH)
 
 # Definitions for the folders
 FRIMAN_ENV_FOLDER = path.join(FRIMAN_FOLDER, "env") # Folder that contains all the installed frida versions
-FRIMAN_BIN_FOLDER = path.join(FRIMAN_FOLDER, "bin") # Folder that links to the currently used frida version
+FRIMAN_CURRENT_FOLDER = path.join(FRIMAN_FOLDER, "current") # Folder that links to the currently used frida version
 FRIMAN_SERVER_FOLDER = path.join(FRIMAN_FOLDER, "server") # Folder that contains the downloaded frida servers
+
+# Definitions for the files
+FRIMAN_CONFIG_FILE = path.join(FRIMAN_FOLDER, "config.json") # Contains the current config (list of tags, version in use)
+
+# Env variables
+FRIMAN_DEBUG = "FRIMAN_DEBUG"
+
+# Github
+GITHUB_API = {
+    'base': "api.github.com",
+    'repos': "/repos",
+    'releases': "/releases",
+    'tags': "/tags"
+}
+
+# Frida
+FRIDA_REPO = "frida/frida"
--- friman/utils/helpers.py
+++ friman/utils/helpers.py
@@ -2,11 +2,19 @@
 This module contains multiple helper function used throughout the codebase.
 """
 import os
-from .definitions import FRIMAN_FOLDER, FRIMAN_BIN_FOLDER, FRIMAN_ENV_FOLDER, FRIMAN_SERVER_FOLDER
+import json
+import http.client
+from datetime import datetime
+from friman.utils import definitions, exceptions
+from friman.utils.logger import frimanlog
 
 def banner(version):
     print()
-    print(f" friman v{version}")
+    print(f"friman v{version}")
+
+def file_exists(filepath: str) -> bool:
+    """Check if a file exists at the given filepath and return a boolean."""
+    return os.path.exists(filepath)
 
 def create_folder_if_missing(path: str) -> None:
     """Utility function to create a folder if missing"""
@@ -15,9 +23,93 @@ def create_folder_if_missing(path: str) -> None:
 
 def ensure_folders():
     # Create the .friman folder in $HOME if missing
-    create_folder_if_missing(FRIMAN_FOLDER)
+    create_folder_if_missing(definitions.FRIMAN_FOLDER)
 
-    # Create the bin, env and server folders within the above one if missing
-    create_folder_if_missing(FRIMAN_BIN_FOLDER)
-    create_folder_if_missing(FRIMAN_ENV_FOLDER)
-    create_folder_if_missing(FRIMAN_SERVER_FOLDER)
+    # Create the env and server folders within the above one if missing
+    create_folder_if_missing(definitions.FRIMAN_ENV_FOLDER)
+    create_folder_if_missing(definitions.FRIMAN_SERVER_FOLDER)
+
+def set_env_if_empty(env_var, env_value):
+    """Set an ENV variable if empty, otherwise don't change it (ENV vars take precedence!)"""
+    os.environ[env_var] = os.environ.get(env_var, env_value)
+
+def get_github_headers():
+    """Returns GitHub authentication headers if a token is available."""
+    github_token = os.getenv('GITHUB_TOKEN')
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.3"
+    }
+    if github_token:
+        headers["Authorization"] = f"token {github_token}"
+    return headers
+
+def make_request(host, url, headers):
+    """Performs an HTTPS GET request and returns the response."""
+    conn = http.client.HTTPSConnection(host)
+    conn.request("GET", url, headers=headers)
+    response = conn.getresponse()
+    data = response.read()
+    conn.close()
+
+    if response.status == 403:
+        raise exceptions.RateLimitException(f"Status code {response.status} - error {data.decode()} - url {url}")
+    elif response.status != 200:
+        raise Exception(f"Status code {response.status} - error {data.decode()} - url {url}")
+
+    return data
+
+def get_all_github_tags(repo: str):
+    """Fetches all tags from the GitHub repository, handling pagination."""
+    tags = []
+    page = 1
+
+    while True:
+        url = f"{definitions.GITHUB_API['repos']}/{repo}{definitions.GITHUB_API['tags']}?page={page}"
+        headers = get_github_headers()
+        
+        data = make_request(definitions.GITHUB_API['base'], url, headers)
+        results = json.loads(data)
+
+        if not results:
+            break
+
+        tags.extend([tag["name"] for tag in results])
+        page += 1
+
+    return tags
+
+def get_github_release_assets(repo: str, tag: str):
+    """Fetches release assets for a given GitHub tag."""
+
+    url = f"{definitions.GITHUB_API['repos']}/{repo}{definitions.GITHUB_API['releases']}{definitions.GITHUB_API['tags']}/{tag}"
+    headers = get_github_headers()
+    
+    try:
+        data = make_request(definitions.GITHUB_API['base'], url, headers)
+        results = json.loads(data)
+    except Exception as e:
+        frimanlog.error(f"Request and parsing JSON failed. {e}")
+        return []
+    
+    return [(asset['name'], asset['browser_download_url']) for asset in results.get('assets', [])]
+
+def get_current_config():
+    if not file_exists(definitions.FRIMAN_CONFIG_FILE):
+        default_config = {
+            "tags": [],
+            "updated_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        }
+        with open(definitions.FRIMAN_CONFIG_FILE, "w") as f:
+            json.dump(default_config, f, ensure_ascii=False, indent=2)
+
+    with open(definitions.FRIMAN_CONFIG_FILE) as f:
+        data = json.load(f)
+        return data
+    
+def get_frida_tags():
+    config = get_current_config()
+    return config["tags"]
+
+def get_installed_versions():
+    installed_versions = [f for f in os.listdir(definitions.FRIMAN_ENV_FOLDER) if os.path.isdir(os.path.join(definitions.FRIMAN_ENV_FOLDER, f))]
+    return installed_versions
