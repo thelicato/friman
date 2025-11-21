11464d455dd4ec49ac089069d25f60697d40be49
thelicato <thelicato@users.noreply.github.com>
2025-11-21 13:54:06 +0100

    release: bump version to 0.1.1

--- friman/__init__.py
+++ friman/__init__.py
@@ -5,7 +5,7 @@ Description: Frida version management tool.
 
 # Project Info
 __title__ = "friman"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __description__ = (
     "Frida version management tool."
 )
